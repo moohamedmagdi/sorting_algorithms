1f3cb957cf6e8ea2bfdf1ab74711b423777d0f3b
moohamedmagdi <moohamedmagdi@gmail.com>
2023-07-21 08:41:20 -0700

    all

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,7 +19,7 @@ void selection_sort(int *array, size_t size)
 		if (smallest != i)
 		{
 		temp = array[smallest];
-		array[smallest] = array[i];
+		array[i] = temp;
 		print_array(array, size);
 		}
 	}
