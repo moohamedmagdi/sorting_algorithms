68d22706a606da881b58514dcd1bc027b7ac922a
moohamedmagdi <moohamedmagdi@gmail.com>
2023-07-21 08:29:51 -0700

    all

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,8 +22,6 @@ void bubble_sort(int *array, size_t size)
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				check = 1;
 				print_array(array, size);
 			}
 	}
